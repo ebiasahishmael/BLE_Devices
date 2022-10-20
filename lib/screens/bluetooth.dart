00fc9e8fe86dafcb70c12fb6c8fb7c8e06caa6b5
maclvin <ebiasahishmael@gmail.com>
2022-10-20 20:21:21 +0200

    Second Commit

--- lib/screens/bluetooth.dart
+++ lib/screens/bluetooth.dart
@@ -1,3 +1,4 @@
+import 'package:ble_device/utils/dimensions.dart';
 import 'package:flutter/material.dart';
 
 class Bluetooth extends StatelessWidget {
@@ -5,8 +6,70 @@ class Bluetooth extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Bluetooth"),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+                left: Dimensions.height_15,
+                top: Dimensions.height_15,
+                right: Dimensions.height_15,
+                bottom: Dimensions.height_6),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.all(
+                        Dimensions.height_20,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(Dimensions.height_10),
+                      ),
+                      backgroundColor: const Color(0xFFF5F6F9)),
+                  child: Column(
+                    children: [
+                      const Icon(Icons.settings_bluetooth_outlined),
+                      SizedBox(
+                        height: Dimensions.height_15,
+                      ),
+                      const Text("Output BLE Error")
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: Dimensions.width_15,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.all(
+                        Dimensions.height_20,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(Dimensions.height_10),
+                      ),
+                      backgroundColor: const Color(0xFFF5F6F9)),
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Icon(Icons.bluetooth_searching),
+                      SizedBox(
+                        height: Dimensions.height_15,
+                      ),
+                      const Text("Search Bluetooth")
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+        ],
+      ),
     );
   }
 }
